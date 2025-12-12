ae6ffdd62c38a5ad28b2ef1c55189b4f3adc15e4
havensc27 <havensc27@gfacademy.org>
2025-12-12 12:54:22 -0500

    commit
    commit

--- PythonProject4/new.py
+++ PythonProject4/new.py
@@ -1,4 +1,6 @@
 from Graphics import *
+from Main import fractal_h_tree
+from Queues import *
 
 Version = "b"
 max_depth = 6
@@ -10,8 +12,12 @@ win = GraphWin("H-Tree Fractal b", 1000, 800)  # Creates a window 1000 x 800 pix
 win.setBackground(color = "white")
 
 def draw_h(win, x1, y1, length, d):
+
+    half_length = length / 2
+
     x_l, x_r = x1 - length, x1 + length
     y_u, y_d = y1 - length, y1 + length
+
     line_width = 1 + (d // 2)
 
     Line(Point(x_l, y1), Point(x_r, y1)).draw(win)
@@ -25,6 +31,16 @@ def draw_h(win, x1, y1, length, d):
         (x_r, y_d)
     ]
 
+if Version == "a":
+    print("Run Version 'a': H-Tree version 1 (Recursive Versions)")
+    fractal_h_tree(max_depth, start_center_x, start_center_y, start_length)
+
+elif Version == "b":
+    print("Run Version 'b': Queue H-Tree")
+
+else:
+    print("Invalid version selected. Just 'a' or 'b'.")
+
 from math import *
 def find_point(p1, distance, angle):
    rad_angle = -1 * radians(angle)
@@ -32,52 +48,28 @@ def find_point(p1, distance, angle):
    ycor = sin(rad_angle) * distance + p1.getY()
    return Point(xcor, ycor)
 
-def fractal_h_tree(d, x1, y1, length):
-    if d == 0:
-        return
-    else:
-        new_center = draw_h(win, x1, y1, length, d)
+q = Queue()
 
+initial_draw = [max_depth, start_center_x, start_center_y, start_length]
+q.enqueue(initial_draw)
+
+while not q.empty():
+
+    current_draw = q.dequeue
+    d = current_draw[0]
+    x = current_draw[1]
+    y = current_draw[2]
+    length = current_draw[3]
+
+    corners = draw_h(win, x, y, length, d)
+
+    if d > 1:
+        new_depth = d - 1
         new_length = length / 2
 
-        for next_x1, next_y1 in new_center:
-            fractal_h_tree(d - 1, next_x1, next_y1, new_length)
+        for new_x, new_y in corners:
+            q.enqueue([new_depth, new_x, new_y, new_length])
 
-
-def queue():
-
-    def h_tree_queue(initial_d, initial_x1, initial_y1, initial_length):
-
-        q = queue()
-
-        q.enqueue([initial_d, initial_x1, initial_y1, initial_length])
-
-        while not q.empty():
-            q0 = q.dequeue()
-            d = q0[0]
-            x1 = q0[1]
-            y1 = q0[2]
-            length = q0[3]
-
-            corners = draw_h(win, x1 , y1, length, d)
-
-            if d > 0:
-                new_length = length / 2
-                for next_x1, next_y1 in corners:
-                    q.enqueue([d - 1, next_x1, next_y1, new_length])
-        return
-
-initial_params = [max_depth, start_center_x / 2, start_center_y / 2, start_length]
-
-if Version == "a":
-    print("Run Version 'a': H-Tree version 1")
-
-elif Version == "b":
-    print("Run Version 'b': Queue H-Tree")
-else:
-    print("Invalid version selected. Just 'a' or 'b'.")
-
-fractal_h_tree(max_depth, start_center_x, start_center_y, start_length)
 win.getMouse()
 win.close()
 
